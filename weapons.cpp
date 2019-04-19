eacaf6db635019dd03e2239db3205809644c287b
andierodin@gmail.com <andierodin@gmail.com>
2019-04-19 17:58:11 +0300

    Add methods for realize ammo functions

--- weapons.cpp
+++ weapons.cpp
@@ -1,6 +1,35 @@
 #include "weapons.h"
 
-Weapons::Weapons()
+
+
+int Ammo::GetDamage()
 {
 
+    return gun_.GetCaliber()*3;
+
+}
+
+int Ammo::GetPenetration()
+{
+  return gun_.GetCaliber();
+}
+
+std::string Ammo::ToString()
+{
+  return type_;
+}
+
+int HECartridge::GetDamage()
+{
+  return (int)(Ammo::GetDamage());
+}
+
+int HEATCartridge::GetDamage()
+{
+  return (int) (Ammo::GetDamage()*0.6);
+}
+
+int APCCatridge::GetDamage()
+{
+  return (int)(Ammo::GetDamage()*0.3);
 }
--- weapons.h
+++ weapons.h
@@ -1,16 +1,37 @@
 #ifndef WEAPONS_H
 #define WEAPONS_H
-
-
-class Weapons
+#include <iostream>
+#include <string>
+#include "gun.h"
+class Ammo
 {
 public:
-Weapons();
+ Ammo(Gun somegun,std::string type):gun_(somegun),type_(type){};
+ virtual int GetDamage();
+ int GetPenetration();
+ std::string ToString();
+ Gun gun_;
 private:
+
+std::string type_;
 
 };
 
+class HECartridge :public Ammo{
+ public: HECartridge(Gun someGun):Ammo(someGun,"HE"){}
+ public: int GetDamage() override;
+};
 
-class
+class HEATCartridge :public Ammo{
+ public :
+  HEATCartridge(Gun someGun):Ammo(someGun,"HEAT"){};
+  int GetDamage () override;
+};
+
+class APCCatridge : public Ammo{
+ public:
+  APCCatridge(Gun someGun):Ammo(someGun,"APC"){}
+  int GetDamage() override;
+};
 
 #endif // WEAPONS_H
