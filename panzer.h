4ec5475adab2e4d0e5407da60dadcc182ae5dfbc
andierodin@gmail.com <andierodin@gmail.com>
2019-04-19 18:11:28 +0300

    Add
    Files for tank class

---
+++ panzer.h
@@ -0,0 +1,28 @@
+#ifndef PANZER_H
+#define PANZER_H
+#include<iostream>
+#include<memory>
+#include<string>
+#include <algorithm>
+#include <list>
+#include"armor.h"
+#include"weapons.h"
+#include"gun.h"
+
+class panzer
+{
+public:
+ panzer(Gun gun):gun_(gun){};
+
+
+private:
+ std::string model_ ;
+ Gun gun_;
+ std::list<Armor> armours;
+ std::list<Ammo> ammos;
+ int health;
+
+
+};
+
+#endif // PANZER_H
